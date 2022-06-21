b8cb5278a76c840fe3a0b099fa6669dbeb300bc9
Ashutosh Aman <69684043+AmanAshutosh@users.noreply.github.com>
2022-06-21 23:18:40 +0530

    Delete 5.3.js

--- Week-5/week5/5.3.js
+++
@@ -1,5 +0,0 @@
-const hasDuplicate = arr => new Set(arr).size !== arr.length;
-
-const myArray = ['p', 'r', 'a', 'd', 'y', 'u', 'm', 'n', 'a'];
-const answer = hasDuplicate(myArray);
-console.log(answer); //This will return true
