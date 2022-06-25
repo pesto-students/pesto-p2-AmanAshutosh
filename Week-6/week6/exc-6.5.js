244a669af1b75c4537ee0bcae55fcb6b357e816b
Ashutosh Aman <69684043+AmanAshutosh@users.noreply.github.com>
2022-06-25 21:12:39 +0530

    Update exc-6.5.js

---
+++ Week-6/week6/exc-6.5.js
@@ -0,0 +1,15 @@
+const findPair = (arr, diff) => {
+    arr.sort((a,b) => a-b);
+    let i = 0, j = 1;
+    while(i < arr.length && j < arr.length) {
+        if(i !== j && arr[j] - arr[i] === diff) {
+            return [arr[i], arr[j]];
+        } else if (arr[j] - arr[i] < diff) {
+            j++;
+        } else {
+            i++;
+        }
+    }
+    return [-1, -1];
+}
+
