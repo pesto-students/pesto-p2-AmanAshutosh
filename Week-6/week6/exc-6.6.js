38d2236e6e563e16c94b17fd3f01022c4680a9db
Ashutosh Aman <69684043+AmanAshutosh@users.noreply.github.com>
2022-06-25 20:49:51 +0530

    Update exc-6.6.js

--- Week-6/week6/exc-6.6.js
+++ Week-6/week6/exc-6.6.js
@@ -1,1 +1,34 @@
+const threeSumClosest = (nums, target) => {
+    //sort the array;
+    nums = nums.sort((a,b)=>a-b);
+    
+    //to store closestSum
+    let closestSum = Infinity;
+  
+    //iterate the array
+    for (let i = 0; i < nums.length; i ++) {
+        //tracker
+        let left = i + 1;
+        let right = nums.length -1;
+      
+        //iterate till we have all the combinations
+        while (left < right) {
+            //sum of the triplets
+            let sum = nums[i] + nums[left] + nums[right];
+            
+            // calc closestSum
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) closestSum = sum;
+          
+            //update the tracker
+            if (sum > target) {
+                right --
+            } else {
+                left ++
+            }
+        }
+    }
+    return closestSum;
+};
 
+//Time Complexity:O(n ^ 2)
+//Space Complexity:O(1)
