8bb6442e70617a67dda5f27efca631ee9c0ded11
Ashutosh Aman <cs18904@glbitm.ac.in>
2022-07-11 18:21:30 +0530

    Week-9

---
+++ Week-9/week9/todo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <span className="title">Todo-List</span> <br />
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
